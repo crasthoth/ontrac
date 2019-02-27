7f5915043025374768ba4585ff65f3b24e2241e9
crasthoth <crasthoth@gmail.com>
2019-02-27 20:01:08 +0000

    nothing

--- ontrac.py
+++ ontrac.py
@@ -23,8 +23,11 @@ import json
 
 # TODO: change this to come in as a list and multithread will break it apart and try each tracking number individually
 # TODO: figure out how the json return happens for multiple tracking numbers
-tracknums = ','.join(sys.argv[1:])
-
+# TODO: Implement error return when no tracking number provided
+if len(sys.argv) > 1:
+    tracknums = ','.join(sys.argv[1:])
+else:
+    sys.exit("No tracking number provided")
 # Much faster, trying to parse immediate json response instead of using PhantomJS
 # TODO: send requests to https://amway.narvar.com/trackinginfo/amway/ontrac?tracking_numbers=C000000000
 def requeststatus(tracknums):
@@ -76,13 +79,15 @@ while c:
         print("UPDATE: {}".format(status))
         print("Sending update to {}".format(local_settings.tophone))
         message = "-\nPackage Update\nOnTrac Number: {}\nStatus: {}".format(tracknums,status)
-        sendupdates(message)
+        #print("Simulate message send: {}".format(message)) # for testing without sending yourself lots of messages
+        sendupdates(message) # comment out to skip sending sms
     if status == 'Delivered':
         c = False
         try:
             os.remove('ghostdriver.log')
         except:
             pass
+        #sys.stdout.flush() # doesn't do anything
         print("Delivered, exiting...")
     else:
         # TIMER BLOCK - to vary the time between server requests
